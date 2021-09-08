e55954bb259a5269b183d173d05a2006471aeca9
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-09-08 16:06:09 +0200

    add create_trips_table.php

---
+++ database/migrations/2021_09_08_125144_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',50);
+            $table->string('where',100);
+            $table->float('cost', 8, 2);
+            $table->date('from');
+            $table->date('to');
+            $table->tinyInteger('aviable',)->unsigned()->default(1);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
